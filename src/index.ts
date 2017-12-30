5974df4584177edb56976de8c25ce59090931108
hejny <me@pavolhejny.com>
2017-12-30 15:10:13 +0100

    + Unfollowing

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ function random(min: number, max: number) {
 
 function tickFollow() {
     //console.groupCollapsed('tick');
-    let follewed = 0;
+    let followed = 0;
     const list = document.body.querySelectorAll('button');
     for (var i = 0, l = list.length; i < l; i++) {
         const buttonElement: HTMLButtonElement = list[i];
@@ -27,14 +27,44 @@ function tickFollow() {
             //console.log(buttonElement);
             buttonElement.click();
             scrollParent(buttonElement, buttonElement.clientHeight*2);
-            follewed++;
+            followed++;
 
             break;
         }
     }
-    console.log(`Followed ${follewed} people.`);
+    console.log(`Followed ${followed} people.`);
+    //console.groupEnd();
+}tickFollow;
+
+
+async function tickUnfollow() {
+
+
+    //await browser.storage.local.get("users");
+
+
+    //console.groupCollapsed('tick');
+    let unfollowed: string[] = [];
+    const list = document.body.querySelectorAll('button');
+    for (var i = list.length-1; i >= 0; i--) {
+        const buttonElement: HTMLButtonElement = list[i];
+        if (buttonElement.innerText === 'Following') {
+            //console.log(buttonElement);
+            buttonElement.click();
+            //scrollParent(buttonElement, buttonElement.clientHeight*2);
+            unfollowed.push(findUserId(buttonElement));
+
+
+            //scrollParent(buttonElement,500);
+
+            break;
+        }
+    }
+    console.log(`Unfollowed ${unfollowed.join(', ')}`);
     //console.groupEnd();
 }
+
+
 
 
 function scrollParent(element: HTMLElement, height: number) {
@@ -49,6 +79,53 @@ function scrollParent(element: HTMLElement, height: number) {
 
     }
 }
+
+
+function _onlyUnique<T>(value: T, index: number, self: T[]): boolean {
+    return self.indexOf(value) === index;
+}
+
+function _findUserIdOnSelfOfChildren(element: HTMLElement):string[] {//todo not optimalisation
+    const userIds:string[] = [];
+    //console.log(element.tagName);
+    if(element.tagName==='A'){
+        const userId = element.getAttribute('href');
+        if(typeof userId === 'string'){
+            //todo regexp
+            userIds.push(userId);
+        }
+    }
+    for(let i=0,l=element.children.length;i<l;i++){
+        const child = element.children[i];
+        for(const userId of _findUserIdOnSelfOfChildren(child as HTMLElement)){
+            userIds.push(userId);
+        }
+    }
+    return userIds.filter( _onlyUnique );;
+}
+
+
+function findUserId(element: HTMLElement):string {
+
+    const userIds = _findUserIdOnSelfOfChildren(element);
+
+    if(userIds.length===0){
+        if(element.parentElement) {
+            return findUserId(element.parentElement);
+        }else{
+            console.log(element);
+            throw new Error('Cant get any user id.');
+        }
+    }else
+    if(userIds.length===1){
+        return userIds[0];
+    }else{
+        console.log(element);
+        throw new Error('Cant get unique user id.');
+    }
+
+}
+
 
 /*function tickScroll() {
     //console.groupCollapsed('tick');
@@ -81,7 +158,8 @@ function clock(tickCb: () => void, delayCb: () => number) {
 }
 
 
-clock(tickFollow, () => random(28, 36) * 1000);
+//clock(tickUnfollow, () => random(28, 36) * 1000);
+clock(tickUnfollow, () => random(12, 22) * 1000);
 
 
 /*try {
