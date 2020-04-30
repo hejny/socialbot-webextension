243403d0d7a4843f91e184fd4d2b35185b818f89
Pavol Hejný <me@pavolhejny.com>
2020-05-01 01:19:58 +0200

    Updates

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@
 
 const CONTACTS_LIMIT = 10000;
 
-function forRandomTime() {
+function forRandomTime(random=20,fixed=1) {
     return new Promise((resolve) => {
         setTimeout(() => {
             resolve();
-        }, Math.random() * 20);
+        }, Math.random() * length+fixed);
     });
 }
 
@@ -18,10 +18,10 @@ class SlackContactsScraper {
         let contacts = '';
         this._contactsNames = [];
 
-        this.workspaceName = $(`.p-classic_nav__team_header__team__name`).text();
+        this.workspaceName = $(`.p-ia__sidebar_header__team_name_text`).text();
 
-        await this._back();
-        await this._scrollTop();
+        //await this._back();
+        //await this._scrollTop();
         for (let i = 0; i < CONTACTS_LIMIT; i++) {
             const contactName = await this._getNextContactName();
             if (!contactName) break;
@@ -29,9 +29,16 @@ class SlackContactsScraper {
             contacts += '\n' + (await this._scrapeContactFromName(contactName));
             contacts = contacts.trim();
             this._contactsNames.push(contactName);
+
         }
-        await this._back();
+        //await this._back();
         return contacts;
+    }
+
+    async _nextPage(){
+        console.log(`Listing next page.`);
+        $(`button[data-qa="browse_page_paginator_forward_btn"]`).click();
+        await forRandomTime(20,2000);
     }
 
     _getLastContactName() {
@@ -42,9 +49,9 @@ class SlackContactsScraper {
         }
     }
 
-    async _getNextContactName(scrolls = 0) {
+    async _getNextContactName(tryNextPage = true) {
         let nextContactName = null;
-        const contactNames = this._scrapeContactNames();
+        const contactNames = this._scrapeNewContactNames();
         if (this._getLastContactName()) {
             const i = contactNames.lastIndexOf(this._getLastContactName());
             nextContactName = contactNames[i + 1];
@@ -55,19 +62,46 @@ class SlackContactsScraper {
             return nextContactName;
         }
 
-        if (scrolls > 5) return null;
-        await this._scroll();
-        return this._getNextContactName(scrolls + 1);
+
+        if(tryNextPage){
+            await this._nextPage()
+            return await this._getNextContactName(false);
+        }
+
+
+        console.log('contactNames',contactNames);
+        console.log('this._getLastContactName()',this._getLastContactName());
+        console.log('nextContactName',nextContactName);
+
+        //if (scrolls > 5) return null;
+        //await this._scroll();
+        //return this._getNextContactName(scrolls + 1);
+
+        return null;
     }
 
+    /*
+    _contactsNamesTmpSet = new Set();
     _scrapeContactNames() {
-        return Array.from($('.p-member_directory__member_row')).map((contactElement) =>
+        for( const contactName of this._scrapeNewContactNames()){
+
+
+
+            if(!this._contactsNamesTmpSet.has(contactName)){
+                this._contactsNamesTmpSet.add(contactName);
+            }
+        }
+        return Array.from(this._contactsNames);
+    }*/
+    _scrapeNewContactNames() {
+        return Array.from($('.p-browse_page_member')).map((contactElement) =>
             $(contactElement)
-                .find(`.c-member_name`)
+                .find(`.p-browse_page_member_card_entity__name_text`)
                 .text(),
         );
     }
 
+    /*
     async _scrollTop() {
         $('.p-member_directory__member_row')[0].parentElement.parentElement.parentElement.parentElement.scrollTop = 0;
         await forRandomTime();
@@ -91,13 +125,29 @@ class SlackContactsScraper {
         }
         await forRandomTime();
     }
+    */
 
     async _scrapeContactFromName(name) {
-        $(`*:contains('${name}')`).click();
-        await forRandomTime();
+        $(`.p-browse_page_member_card_entity .p-browse_page_member_card_entity__name_text span:contains('${name}')`).css('border','1px solid red').click();
+        await forRandomTime(20,100);
 
-        const FN = $(`.p-member_profile_name__title`).text();
+        const TITLE = $(`.p-member_profile_name__title`).text();
+        const DISPLAY_NAME = $(`.p-member_profile_field__label:contains('Display name')`)
+        .next()
+        .text();
+        let FN = DISPLAY_NAME||TITLE;
+        let error = '';
+
+        if(FN!==name){
+            console.warn(`Want to scrape "${name}" but scraped "${FN}".`);
+            error=`Probbaply not scraped correctly from "${window.location}". Want to scrape "${name}" but scraped "${FN}".`
+            FN=name;
+        }
+
         const ROLE = $(`.p-member_profile_name__subtitle`).text();
+        const STATUS = $(`.p-member_profile_field__label:contains('Status')`)
+            .next()
+            .text();
         const TEL = $(`.p-member_profile_field__label:contains('Phone number')`)
             .next()
             .text();
@@ -110,13 +160,13 @@ BEGIN:VCARD
 VERSION:3.0
 FN:${FN}
 TEL;TYPE=CELL:${TEL}
-NOTE:Imported from ${this.workspaceName} Slack.
+NOTE:${STATUS?STATUS+'\\n':''}${error?error+'\\n':''}Imported from ${this.workspaceName} Slack.
 EMAIL:${EMAIL}
 ROLE:${ROLE}
 END:VCARD
         `.trim();
 
-        await this._back();
+        //await this._back();
 
         return contact;
     }
@@ -140,10 +190,12 @@ $(() => {
     `);
 
     $('#download-contacts').click(async () => {
-        if (!window.location.toString().match(/\/directory$/)) {
-            if (confirm(`You need to be at .../directory path with directory opened. Do you want to go there now?`)) {
-                window.location = window.location + '/directory';
-            }
+        if (!window.location.toString().match(/\/browse-people/)) {
+            /*if (confirm(`You need to be at .../browse-people path with directory opened. Do you want to go there now?`)) {
+                window.location = window.location + '/browse-people';
+            }*/
+
+            alert(`Please go to /browse-people and check "Show deactivated accounts" in filter.`)
             return;
         }
 
